94698f3427051ce81b3ca88b773cfd98c7be0c50
Cesar Jorahua <cesarjorahua@gmail.com>
2026-01-29 00:55:09 -0600

    feat: Score testing
    - Previous implementation of what the **task 2** requires

--- Assets/Scripts/ScoreAndMoveManager.cs
+++ Assets/Scripts/ScoreAndMoveManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreAndMoveManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int startingMoves = 5;
+
+    [Space(10)]
+    [Header("Canvases")]
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private GameObject presentationScreen;
+
+    [Space(10)]
+    [Header("TMP Objects")]
+    [SerializeField] private TMPro.TextMeshProUGUI movesText;
+    [SerializeField] private TMPro.TextMeshProUGUI scoreText;
+
+    private int currentMoves;
+
+    private int score = 0;
+
+    private void Awake()
     {
-        
+        currentMoves = startingMoves;
+        if(gameOverScreen==null)
+            Debug.LogError($"[{GetType()}] GameOver screen not found! Make sure there is attached to this component.", this);
+        if(presentationScreen==null)
+            Debug.LogError($"[{GetType()}] Presentation screen not found! Make sure there is attached to this component.", this);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MakeMove()
     {
-        
+        currentMoves--;
+        score+=10;
+        Debug.Log($"[{GetType()}] Current moves:  " + currentMoves);
+        Debug.Log($"[{GetType()}] Score: " + score);
+        if (currentMoves <= 0)
+        {
+            gameOverScreen.SetActive(true);
+            //Disable interaction with the presentation canvas
+            presentationScreen.GetComponent<GraphicRaycaster>().enabled = false;
+            Debug.Log($"[{GetType()}] Game over restart");
+        }
     }
+
+    public void ResetGameplay()
+    {
+        currentMoves = startingMoves;
+        score = 0;
+        gameOverScreen.SetActive(false);
+    }
+
 }
