f48077107b3e9766037c17983f053ad8d18bb991
Cesar Jorahua <cesarjorahua@gmail.com>
2026-02-01 22:37:40 -0600

    feat: Added presenters
    - Implemented to handle the visuals and separate them from the managers (logic)

---
+++ Assets/Scripts/Presenters/MovePresenter.cs
@@ -0,0 +1,77 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using ProjectBlocky.Managers;
+
+namespace ProjectBlocky.Presenters
+{
+    public class MovePresenter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text movesText;
+        [SerializeField] private GameObject gameOverScreen;
+        [SerializeField] private Button restartButton;
+
+        private MoveManager _moveManager;
+        private GameManager _gameManager;
+
+        [Inject]
+        public void Construct(MoveManager moveManager, GameManager gameManager)
+        {
+            _moveManager = moveManager;
+            _gameManager = gameManager;
+        }
+
+        private void Awake()
+        {
+            // Bind the restart button click to the GameManager's restart method
+            if (restartButton != null)
+            {
+                restartButton.onClick.AddListener(OnRestartButtonClicked);
+            }
+        }
+
+        private void OnEnable()
+        {
+            _moveManager.OnMovesChanged += UpdateMoves;
+            _moveManager.OnGameOver += ShowGameOver;
+            _gameManager.OnGameRestart += HideGameOver;
+        }
+
+        private void OnDisable()
+        {
+            _moveManager.OnMovesChanged -= UpdateMoves;
+            _moveManager.OnGameOver -= ShowGameOver;
+            _gameManager.OnGameRestart -= HideGameOver;
+        }
+
+        private void OnDestroy()
+        {
+            // Clean up button listener to prevent memory leaks
+            if (restartButton != null)
+            {
+                restartButton.onClick.RemoveListener(OnRestartButtonClicked);
+            }
+        }
+
+        private void UpdateMoves(int moves)
+        {
+            movesText.text = moves.ToString();
+        }
+
+        private void ShowGameOver()
+        {
+            gameOverScreen.SetActive(true);
+        }
+
+        private void HideGameOver()
+        {
+            gameOverScreen.SetActive(false);
+        }
+
+        private void OnRestartButtonClicked()
+        {
+            _gameManager.RestartGame();
+        }
+    }
+}
---
+++ Assets/Scripts/Presenters/ScorePresenter.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using Zenject;
+using ProjectBlocky.Managers;
+
+namespace ProjectBlocky.Presenters
+{
+    public class ScorePresenter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text scoreText;
+
+        private ScoreManager _manager;
+
+        [Inject]
+        public void Construct(ScoreManager manager)
+        {
+            _manager = manager;
+        }
+
+        private void OnEnable()
+        {
+            _manager.OnScoreChanged += UpdateScore;
+        }
+
+        private void OnDisable()
+        {
+            _manager.OnScoreChanged -= UpdateScore;
+        }
+
+        private void UpdateScore(int score)
+        {
+            scoreText.text = score.ToString();
+        }
+    }
+}
